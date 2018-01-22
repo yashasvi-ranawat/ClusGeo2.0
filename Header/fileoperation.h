b41e36d678763182e97c37ee08d011d2db2c6c0e
Yashasvi S. Ranawat <yashasvi.ranawat@gmail.com>
2018-01-22 12:50:32 +0200

    created bleach version; updated file operations

--- Header/fileoperation.h
+++ Header/fileoperation.h
@@ -81,4 +81,77 @@ string* getType(char* myFile){
 
 return mytype;
 }
+//--------------------------------------------------------------------------
+
+mat getChgType(char* myFile){
+
+  ifstream myfilein(myFile);
+  string line;
+  double count, posX,posY,posZ;
+
+  mat A(4,4);
+
+  int j=0;
+
+    while (getline(myfilein, line))
+    {
+        istringstream ss(line);
+
+        string name;
+        ss >> count >> posX>> posY>> posZ;
+        if (j >1) {
+        A(j - 2,0) = count;
+        A(j - 2,1) = posX;
+        A(j - 2,2) = posY;
+        A(j - 2,3) = posZ;
+     }
+
+        j++; 
+        if (j>5){
+        break;
+        }
+    }
+myfilein.close();
+return A;
+
+  
+}
+//--------------------------------------------------------------------------
+
+cube getChgVal(char* myFile, mat chgType){
+
+  ifstream myfilein(myFile);
+  string line;
+  int totvoxl = chgType(1,0)*chgType(2,0)*chgType(3,0);
+  int numlines = floor(totvoxl/10.0)+1;
+  double pos[10*numlines];
+
+  cube A = zeros<cube>(int(chgType(1,0)),int(chgType(2,0)),int(chgType(3,0)));
+  
+  int i=0;
+  int j=0;
+  int k=0;
+
+    while (getline(myfilein, line))
+    {
+        if (j >4+int(chgType(0,0))) {
+        istringstream ss(line);
+        ss >> pos[(10*k)] >> pos[(10*k+1)] >> pos[(10*k+2)] >> pos[(10*k+3)] >> pos[(10*k+4)] >> pos[(10*k+5)] >> pos[(10*k+6)] >> pos[(10*k+7)] >> pos[(10*k+8)] >> pos[(10*k+9)];
+        k++;
+     }
+
+        j++; 
+    }
+  for (k=0; k<chgType(3,0);k++){  
+      for (j=0; j<chgType(2,0);j++){
+          for (i=0; i<chgType(1,0);i++){
+          A.at(i,j,k) = pos[(i+j+k)];
+          }
+      }
+  }
+myfilein.close();
+return A;
+
+  
+}
 #endif // 
